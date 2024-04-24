3d0b3706a95ca55c3b9fe11df46023283fd4687d
damdam48 <damdam.formation@gmail.com>
2024-04-24 14:09:27 +0200

    feat(User): add login and logout configuration

---
+++ src/Controller/Security/SecurityControlleurController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Security\Model\Authenticator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityControlleurController extends AbstractController
+{
+    #[Route('/login', name: 'app.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authUtils): Response
+    {
+        return $this->render('Security/login.html.twig', [
+            'error' => $authUtils->getLastAuthenticationError(),
+            'lastUsername' => $authUtils->getLastUsername(),
+        ]);
+    }
+}
