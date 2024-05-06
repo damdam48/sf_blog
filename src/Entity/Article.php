1967918da87cdca23e1c235424e82948875e6af9
damdam48 <damdam.formation@gmail.com>
2024-05-06 16:05:42 +0200

    feat: article add entity and constrant

---
+++ src/Entity/Article.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ArticleRepository;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQUE_TITLE_ARTICLE', fields: ['title'])]
+#[UniqueEntity(fields: ['title'], message: 'Le titre est deja utilisé par un autre article')]
+#[ORM\HasLifecycleCallbacks]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min: 5,
+        max: 255
+    )]
+
+    #[Gedmo\Slug(fields: ['title'])]
+    private ?string $slug = null;
+
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?bool $enable = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isEnable(): ?bool
+    {
+        return $this->enable;
+    }
+
+    public function setEnable(bool $enable): static
+    {
+        $this->enable = $enable;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+    #[ORM\PrePersist]
+    public function autoSetCreatedAt(): static
+    {
+        $this->createdAt = new DateTimeImmutable();
+
+        return $this;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    
+    #[ORM\PreUpdate]
+    public function autoSetUpdatedAt(): static
+    {
+        $this->updatedAt = new DateTimeImmutable();
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
