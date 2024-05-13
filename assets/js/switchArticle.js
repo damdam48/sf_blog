c1a1307f5722e38125216407633f97bf0c27fa3b
damdam48 <damdam.formation@gmail.com>
2024-05-13 16:15:23 +0200

    feat: categorie separation du js togle

--- assets/js/switchArticle.js
+++ assets/js/switchArticle.js
@@ -1,38 +1,9 @@
+import { sendVisibilityRequest } from './sendVisibilityRequest';
 document.querySelectorAll('input[data-switch-article-id]')
     .forEach(input => {
         input.addEventListener('change', async (e) => {
             const id = e.currentTarget.dataset.switchArticleId;
-
-            const response = await fetch(`/admin/articles/${id}/switch`);
- 
-            const data = await response.json();
-
-            if (response.ok) {
-                const card = e.target.closest('.card');
-                const label = e.target.nextElementSibling;
-
-                if (data.enable) {
-                    card.classList.replace('border-danger', 'border-success');
-                    label.classList.replace('text-danger', 'text-success');
-                    label.innerText = 'Actif';
-                } else {
-                    card.classList.replace('border-success', 'border-danger');
-                    label.classList.replace('text-success', 'text-danger');
-                    label.innerText = 'Inactif';
-
-                }
-
-            } else {
-                if (document.querySelector('.alert.alert-danger')) {
-                    document.querySelector('.alert.alert-danger').remove();
-                }
-                const alert = document.createElement('div');
-                alert.classList.add('alert', 'alert-danger');
-
-                alert.innerText = data.message;
-
-                document.querySelector('main').prepend(alert);
-            }
+            sendVisibilityRequest(`/admin/articles/${id}/switch`, e.target);
 
         });
     });
--- assets/js/switchCategorie.js
+++ assets/js/switchCategorie.js
@@ -1,38 +1,11 @@
+import { sendVisibilityRequest } from './sendVisibilityRequest';
+
 document.querySelectorAll('input[data-switch-categorie-id]')
     .forEach(input => {
         input.addEventListener('change', async (e) => {
             const id = e.currentTarget.dataset.switchCategorieId;
+            sendVisibilityRequest(`/admin/categories/${id}/switch`, e.target);
 
-            const response = await fetch(`/admin/categories/${id}/switch`);
- 
-            const data = await response.json();
-
-            if (response.ok) {
-                const card = e.target.closest('.card');
-                const label = e.target.nextElementSibling;
-
-                if (data.enable) {
-                    card.classList.replace('border-danger', 'border-success');
-                    label.classList.replace('text-danger', 'text-success');
-                    label.innerText = 'Actif';
-                } else {
-                    card.classList.replace('border-success', 'border-danger');
-                    label.classList.replace('text-success', 'text-danger');
-                    label.innerText = 'Inactif';
-
-                }
-
-            } else {
-                if (document.querySelector('.alert.alert-danger')) {
-                    document.querySelector('.alert.alert-danger').remove();
-                }
-                const alert = document.createElement('div');
-                alert.classList.add('alert', 'alert-danger');
-
-                alert.innerText = data.message;
-
-                document.querySelector('main').prepend(alert);
-            }
 
         });
     });
